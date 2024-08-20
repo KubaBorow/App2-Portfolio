b0437712d9366d19f3abb99f769850131f91302d
KubaBorow <jakubrcam@gmail.com>
2024-08-20 22:48:01 +0200

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.jpg", width=400)
 
 with col2:
     st.title("Jakub Borowicki")
@@ -16,3 +16,8 @@ with col2:
     Through practice and experimentation, I'm steadily building confidence in my coding abilities. 
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have build in Python. Feel free to contact me!
+"""
+st.write(content2)
