b6ce4fd275142ad8abec3d0e91fefd67937fd037
KubaBorow <jakubrcam@gmail.com>
2024-08-20 22:31:26 +0200

    Create columns, image and description.

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Jakub Borowicki")
+    content = """
+    Hi, I am Kuba! As a beginner Python programmer, I'm just starting to grasp the fundamentals of coding, 
+    exploring concepts like variables, loops, and functions. 
+    I'm eager to apply my growing knowledge by writing simple programs and solving basic problems. 
+    Through practice and experimentation, I'm steadily building confidence in my coding abilities. 
+    """
+    st.info(content)
