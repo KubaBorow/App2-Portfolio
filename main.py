421a7028058996cb08bfde6650e6c9fe66b48a1d
KubaBorow <jakubrcam@gmail.com>
2024-08-24 14:26:52 +0200

    Render the titles, changing photo.

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import streamlit as st
+import pandas
+
+
+# Pandasa używamy np do odczytu plików csv
+
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg", width=400)
+    st.image("images/photo.jpg")
 
 with col2:
     st.title("Jakub Borowicki")
     content = """
-    Hi, I am Kuba! As a beginner Python programmer, I'm just starting to grasp the fundamentals of coding, 
-    exploring concepts like variables, loops, and functions. 
-    I'm eager to apply my growing knowledge by writing simple programs and solving basic problems. 
-    Through practice and experimentation, I'm steadily building confidence in my coding abilities. 
+    Hi, I am Kuba! As a beginner Python programmer, I'm just starting to grasp the fundamentals of coding,
+    exploring concepts like variables, loops, and functions.
+    I'm eager to apply my growing knowledge by writing simple programs and solving basic problems.
+    Through practice and experimentation, I'm steadily building confidence in my coding abilities.
     """
     st.info(content)
 
@@ -21,3 +26,16 @@ content2 = """
 Below you can find some of the apps I have build in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
